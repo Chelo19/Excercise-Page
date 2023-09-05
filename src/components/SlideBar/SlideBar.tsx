b53f4f8a3692a54cbe1c8591393020e4baa5d497
Chelo <marcelodeleongo@gmail.com>
2023-09-05 16:55:46 -0600

    Inicio de navbar

--- src/components/SlideBar/SlideBar.tsx
+++ src/components/SlideBar/SlideBar.tsx
@@ -4,9 +4,9 @@ function SlideBar(){
     return(
         <div className='slide_bar'>
             <div className='slide_bar_container'>
-                <div className='slide_bar_item'>item1</div>
-                <div className='slide_bar_item'>item1</div>
-                <div className='slide_bar_item'>item1</div>
+                <div className='slide_bar_item'>Upper Body</div>
+                <div className='slide_bar_item'>Lower Body</div>
+                <div className='slide_bar_item'>Cardio</div>
             </div>
         </div>
     );
--- src/pages/FreeSearch/FreeSearch.tsx
+++ src/pages/FreeSearch/FreeSearch.tsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/Header";
 import SlideBar from "../../components/SlideBar/SlideBar";
+import NavBar from "../../components/NavBar/NavBar";
 
 function FreeSearch(){
     return(
         <>
             <Header/>
+            <NavBar/>
             <SlideBar/>
         </>
     );
